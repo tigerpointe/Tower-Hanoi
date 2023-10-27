f7fc62206bf0b2517bfb07d5564c6063eaf861ca
tigerpointe <102432718+tigerpointe@users.noreply.github.com>
2023-10-27 09:23:46 -0400

    Add files via upload

--- Start-TowerHanoi.ps1
+++ Start-TowerHanoi.ps1
@@ -139,9 +139,9 @@ function Start-Gameplay
         $outC = "|".PadLeft($data["height"]);
         $outC = $outC + " ".PadRight($data["height"]);
       }
-      Write-Host -Object "  :$($outA)$($outB)$($outC)";
+      Write-Host -Object "  : $($outA)$($outB)$($outC)";
     }
-    Write-Host -NoNewline -Object "  :";
+    Write-Host -NoNewline -Object "  : ";
     Write-Host -NoNewline -Object "A".PadLeft($data["height"]);
     Write-Host -NoNewline -Object " ".PadRight($data["height"]);
     Write-Host -NoNewline -Object "B".PadLeft($data["height"]);
